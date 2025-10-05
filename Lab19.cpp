6376015807440b4864d40f869f84feb6c8103fa4
mansoornoori18 <mnoori926@insite.4cd.edu>
2025-10-04 22:49:33 -0700

    update

--- Lab19.cpp
+++ Lab19.cpp
@@ -19,11 +19,13 @@ class Movie{
         string comment;
         ReviewNode* next;   // Pointer to the next review in the list
     };
+    ReviewNode* head;       // Pointer to the first review in the linked list
 
     public:
     // Constructor: sets the movie title and initializes head to nullptr
     Movie(const string& title) : title(title), head(nullptr){}
-
+    
+    // Destructor: deletes all review nodes to free memory
     ~Movie(){
         while (head != nullptr){
             ReviewNode* temp = head;
@@ -31,6 +33,16 @@ class Movie{
             delete temp;
         }
     }
+    // Adds a new review to the head of the linked list
+    void addReview(double rating, const string& comment){
+        ReviewNode* newNode = new ReviewNode{ rating, comment, head };
+        head = newNode;
+    }
+
+    void printReviews() const {
+        
+    }
+
 
 
 };
