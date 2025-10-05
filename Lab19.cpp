1f0162ad88022cbb90c9834523a5329f2ed99578
mansoornoori18 <mnoori926@insite.4cd.edu>
2025-10-04 22:26:25 -0700

    update

--- Lab19.cpp
+++ Lab19.cpp
@@ -1,6 +1,19 @@
-#include<iostream>
+#include<iostream>  // for input/output cin/cout  // COMSC-210 | Lab 19 | Mansoor Noori
+#include<fstream>   // for reading from file      // IDE Visual Studio,
+#include<vector>    // for using dynamic array 
+#include<iomanip>   // for formatting output setprecision
+#include<string>    // for using the strings 
+#include<ctime>     // for generating random number using time
+
 
 using namespace std;
+
+// movie class
+class Movie{
+    private:
+    string title;
+
+};
 
 int main(){
 
@@ -9,5 +22,5 @@ int main(){
     cout << "movies review project\n";
 
     return 0;
-    
+
 }
